1b8eedd56d7811ca2aa0008384b902b753e68b92
Trd-vandolph <trd.vandolph@gmail.com>
2015-07-29 23:15:10 +0800

    remove errors and fix conflicts

--- fuel/app/views/template.php
+++ fuel/app/views/template.php
@@ -16,63 +16,26 @@
 	<meta property="og:image" content="http://bootcamp.csp.edu/assets/img/success1.png" />
 	<link rel="shortcut icon" href="/assets/img/common/game_b.ico">
 
-	<? echo Asset::css( "template.css"); ?>
-	<? echo Asset::js( "reum.js"); ?>
-
-
-	<!-- This site is optimized with the Yoast WordPress SEO plugin v1.7.4 - https://yoast.com/wordpress/plugins/seo/ -->
-	<!-- link rel="canonical" href="index.html" /-->
-	<!--script type="application/ld+json">{ "@context": "http://schema.org", "@type": "WebSite", "url": "http://bootcamp.csp.edu/", "potentialAction": { "@type": "SearchAction", "target": "http://bootcamp.csp.edu/?s={search_term}", "query-input": "required name=search_term" } }</script-->
-	<!-- / Yoast WordPress SEO plugin. -->
-	<!--link rel="alternate" type="application/rss+xml" title="CSP Bootcamp &raquo; Feed" href="feed/index.html" />
-<link rel="alternate" type="application/rss+xml" title="CSP Bootcamp &raquo; Comments Feed" href="comments/feed/index.html" />
-<link rel="alternate" type="application/rss+xml" title="CSP Bootcamp &raquo; Homepage &#8211; Bootcamp w/ Video Comments Feed" href="homepage-bootcamp-w-video/feed/index.html" /-->
-	<!--link rel='stylesheet' id='flick-css' href='wp-content/plugins/mailchimp_/css/flick/flickf39e.css?ver=4.0.1' type='text/css' media='all' /-->
-	<!--link rel='stylesheet' id='mailchimpSF_main_css-css' href='indexd153.html?mcsf_action=main_css&amp;ver=4.0.1' type='text/css' media='all' /-->
-	<!--[if IE]>
-<link rel='stylesheet' id='mailchimpSF_ie_css-css'  href='http://bootcamp.csp.edu/wp-content/plugins/mailchimp/css/ie.css?ver=4.0.1' type='text/css' media='all' />
-<![endif]-->
-	<? echo Asset::css( "wpbakery/js_composer/assets/css/js_composer_front1ac1.css"); ?>
+	<? echo Asset::css("template.css"); ?>
+	<? echo Asset::js("reum.js"); ?>
+	<? echo Asset::css("wpbakery/js_composer/assets/css/js_composer_front1ac1.css"); ?>
 	<link rel='stylesheet' id='options_typography_Open+Sans-300-css' href='https://fonts.googleapis.com/css?family=Open+Sans:300' type='text/css' media='all' />
 	<!--? echo Asset::css("plugins/contact-form-7/includes/css/styles7e2e.css"); ?-->
-	<? echo Asset::css( "plugins/wp-countdown-timer/public/assets/css/public8a54.css"); ?>
-	<? echo Asset::css( "rgs11b8.css"); ?>
+	<? echo Asset::css("plugins/wp-countdown-timer/public/assets/css/public8a54.css"); ?>
+	<? echo Asset::css("rgs11b8.css"); ?>
 	<link rel='stylesheet' id='font-awesome-css' href='wp-content/themes/salient/css/font-awesome.minf39e.css?ver=4.0.1' type='text/css' media='all' />
 	<!-- <? echo Asset::css("font-awesome.minf39e.css"); ?> -->
-	<? echo Asset::css( "steadysetsf39e.css"); ?>
-	<? echo Asset::css( "lineconf39e.css"); ?>
+	<? echo Asset::css("steadysetsf39e.css"); ?>
+	<? echo Asset::css("lineconf39e.css"); ?>
 	<link rel='stylesheet' id='main-styles-css' href='wp-content/themes/salient/style11b8.css?ver=4.5' type='text/css' media='all' />
-	<!-- <? echo Asset::css("style11b8.css"); ?> -->
-	<!--[if lt IE 9]>
-<link rel='stylesheet' id='nectar-ie8-css'  href='http://bootcamp.csp.edu/wp-content/themes/salient/css/ie8.css?ver=4.0.1' type='text/css' media='all' />
-<![endif]-->
-	<link rel='stylesheet' id='responsive-css' href='wp-content/themes/salient/css/responsive11b8.css?ver=4.5' type='text/css' media='all' />
-	<!--link rel='stylesheet' id='dashicons-css' href='wp-includes/css/dashicons.minf39e.css?ver=4.0.1' type='text/css' media='all' /-->
+	<? echo Asset::css("responsive11b80.css"); ?>
 	<link rel='stylesheet' id='thickbox-css' href='wp-includes/js/thickbox/thickboxf39e.css?ver=4.0.1' type='text/css' media='all' />
-	<!--script src="//code.jquery.com/jquery-1.11.3.min.js"></script-->
-	<!--script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script-->
 	<script type='text/javascript' src='wp-includes/js/jquery/jquery90f9.js?ver=1.11.1'></script>
 	<script type='text/javascript' src='wp-includes/js/jquery/jquery-migrate.min1576.js?ver=1.2.1'></script>
-	<!--script type='text/javascript' src='wp-content/plugins/mailchimp/js/scrollTo330a.js?ver=1.4.1'></script-->
-	<!--script type='text/javascript' src='wp-content/plugins/contact-form-7/includes/js/jquery.form.min9d5c.js?ver=3.50.0-2014.02.05'></script-->
-	<!--script type='text/javascript'>
-    /* <![CDATA[ */
-    var mailchimpSF = {
-        "ajax_url": "http:\/\/bootcamp.csp.edu\/"
-    };
-    /* ]]> */
-</script>
-<!--script type='text/javascript' src='wp-content/plugins/mailchimp/js/mailchimp330a.js?ver=1.4.1'></script-->
 	<script type='text/javascript' src='wp-includes/js/jquery/ui/jquery.ui.core.min2c18.js?ver=1.10.4'></script>
-	<!--script type='text/javascript' src='wp-content/plugins/mailchimp_/js/datepickerf39e.js?ver=4.0.1'></script-->
-	<script type='text/javascript' src='wp-content/plugins/wp-countdown-timer/public/assets/js/public8a54.js?ver=1.0.0'></script>
 	<script type='text/javascript' src='wp-content/themes/salient/js/modernizr61da.js?ver=2.6.2'></script>
-	<!--link rel="EditURI" type="application/rsd+xml" title="RSD" href="xmlrpc0db0.html?rsd" /-->
-	<!--link rel="wlwmanifest" type="application/wlwmanifest+xml" href="wp-includes/wlwmanifest.xml" /-->
-	<!--link rel='shortlink' href='index.html' /-->
-	<? echo Asset::js( "top.jquery.js"); ?>
-	<? echo Asset::js( "top.GoogleAnalyticsObject.js"); ?>
-
+	<? echo Asset::js("top.jquery.js"); ?>
+	<? echo Asset::js("top.GoogleAnalyticsObject.js"); ?>
 	<title> Game-BootCamp </title>
 </head>
 
@@ -122,11 +85,10 @@
 								</li>
 								<li id="menu-item-2722" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2722"><a href="#contact/index.html">Course Fee</a>
 								</li>
+								<li id="menu-item-2721" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2721"><a href="#apply-now/index.html">How to Join</a>
+								</li>
 								<li id="menu-item-2721" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2721"><a href="#apply-now/index.html">FAQ</a>
 								</li>
-								<!--li id="search-btn">
-                                <div><a href="#searchbox"><span class="icon-salient-search" aria-hidden="true"></span></a></div>
-                            </li-->
 							</ul>
 						</nav>
 					</div>
@@ -146,13 +108,10 @@
 				</li>
 				<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2722"><a href="#contact/index.html">Course Fee</a>
 				</li>
+				<li id="menu-item-2721" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2721"><a href="#apply-now/index.html">How to Join</a>
+				</li>
 				<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2721"><a href="#apply-now/index.html">FAQ</a>
 				</li>
-				<!--li id="mobile-search">
-                <form action="http://bootcamp.csp.edu/" method="GET">
-                    <input type="text" name="s" value="" placeholder="Search.." />
-                </form>
-            </li-->
 			</ul>
 		</div>
 	</div>
@@ -195,69 +154,19 @@
 		</div>
 	</div>
 	<a id="to-top"><i class="icon-angle-up"></i></a>
-	<!--script type='text/javascript'>
-    /* <![CDATA[ */
-    var _wpcf7 = {
-        "loaderUrl": "http:\/\/bootcamp.csp.edu\/wp-content\/plugins\/contact-form-7\/images\/ajax-loader.gif",
-        "sending": "Sending ..."
-    };
-    /* ]]> */
-</script-->
-	<!--script type='text/javascript' src='wp-content/plugins/contact-form-7/includes/js/scripts7e2e.js?ver=3.8.1'></script-->
 	<? echo Asset::js( "top.jquery.js"); ?>
 	<script type='text/javascript' src='wp-content/themes/salient/js/superfishb493.js?ver=1.4.8'></script>
-	<!--script type='text/javascript' src='wp-content/themes/salient/js/respond4963.js?ver=1.1'></script-->
 	<script type='text/javascript' src='wp-content/themes/salient/js/nicescrolle248.js?ver=3.5.4'></script>
 	<script type='text/javascript' src='wp-content/themes/salient/js/sticky5152.js?ver=1.0'></script>
 	<script type='text/javascript' src='wp-content/themes/salient/js/prettyPhoto11b8.js?ver=4.5'></script>
-	<!--script type='text/javascript' src='wp-content/themes/salient/wpbakery/js_composer/assets/lib/flexslider/jquery.flexslider-min1ac1.js?ver=3.7.3'></script-->
-	<!--script type='text/javascript' src='wp-content/themes/salient/js/isotope.mind5f7.js?ver=2.0'></script-->
-	<!--script type='text/javascript' src='wp-content/themes/salient/js/carouFredSel.minae48.js?ver=6.2'></script-->
 	<script type='text/javascript' src='wp-content/themes/salient/js/appear5152.js?ver=1.0'></script>
 	<script type='text/javascript' src='wp-content/themes/salient/js/init11b8.js?ver=4.5'></script>
-	<!--script type='text/javascript' src='wp-content/themes/salient/js/nectar-slider8686.js?ver=4.5.1'></script-->
-	<!--script type='text/javascript' src='wp-includes/js/comment-reply.minf39e.js?ver=4.0.1'></script-->
-	<!--script type='text/javascript'>
-    /* <![CDATA[ */
-    var nectarLove = {
-        "ajaxurl": "http:\/\/bootcamp.csp.edu\/wp-admin\/admin-ajax.php",
-        "postID": "3227",
-        "rooturl": "http:\/\/bootcamp.csp.edu",
-        "pluginPages": [],
-        "disqusComments": "false"
-    };
-    /* ]]> */
-</script-->
-	<!--script type='text/javascript' src='wp-content/themes/salient/nectar/love/js/nectar-love5152.js?ver=1.0'></script>
-<script type='text/javascript' src='wp-content/themes/salient/wpbakery/js_composer/assets/js/js_composer_front1ac1.js?ver=3.7.3'></script>
-<script type='text/javascript'>
-    /* <![CDATA[ */
-    var thickboxL10n = {
-        "next": "Next >",
-        "prev": "< Prev",
-        "image": "Image",
-        "of": "of",
-        "close": "Close",
-        "noiframes": "This feature requires inline frames. You have iframes disabled or your browser does not support them.",
-        "loadingAnimation": "http:\/\/bootcamp.csp.edu\/wp-includes\/js\/thickbox\/loadingAnimation.gif"
-    };
-    /* ]]> */
-</script-->
-	<!--script type='text/javascript' src='wp-includes/js/thickbox/thickboxab87.js?ver=3.1-20121105'></script-->
-	<!--script type="text/javascript">
-    window.NREUM || (NREUM = {});
-    NREUM.info = {
-        "beacon": "bam.nr-data.net",
-        "licenseKey": "8af88434bf",
-        "applicationID": "11972078",
-        "transactionName": "ZFYHMkNWWBFWVkMNCV0cJAVFXlkMGEVWAwM=",
-        "queueTime": 0,
-        "applicationTime": 333,
-        "atts": "SBEERAtMSx8=",
-        "errorBeacon": "bam.nr-data.net",
-        "agent": "js-agent.newrelic.com\/nr-632.min.js"
-    }
-</script-->
+	<script type='text/javascript'>
+	/* <![CDATA[ */
+	var nectarLove = {"ajaxurl":"http:\/\/bootcamp.csp.edu\/wp-admin\/admin-ajax.php","postID":"3227","rooturl":"http:\/\/bootcamp.csp.edu","pluginPages":[],"disqusComments":"false"};
+	/* ]]> */
+	</script>
+	<script type='text/javascript' src='wp-content/themes/salient/nectar/love/js/nectar-love5152.js?ver=1.0'></script>
 </body>
 
 </html>
