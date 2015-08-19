743fe5e3ba172bbc44ed7733383d10d5f355f8fd
trd-leoalexander <trd.leoalexander@gmail.com>
2015-08-19 17:47:39 +0800

    [courseFee&faq] created this pages, only one left: try it yourself

--- fuel/app/views/coursefee.php
+++ fuel/app/views/coursefee.php
@@ -1,6 +1,49 @@
-<section id="contents" class="course">
-	<div class="course-content content-text">
+<section id="contents" class="coursefee">
+	<div class="coursefee-content content-text">
 		<div id="content">
+			<p class="sub-title">Course Fee</p>
+			<table cellspacing="0" id="table-coursefee">
+				<thead>
+					<tr>
+						<th>Subject</th>
+						<th>No. of Lesson</th>
+						<th>Term</th>
+						<th>Course Fee</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>enchant.js</td>
+						<td>12</td>
+						<td>3 months</td>
+						<td>
+							<table id="inside-coursefee">
+								<tbody>
+									<tr>
+										<td>One-time payment</td>
+										<td>US$300</td>
+									</tr>
+									<tr>
+										<td>Installment Payment</td>
+										<td>US$110 x 3 times</td>
+									</tr>
+								</tbody>
+							</table>
+						</td>
+					</tr>
+				</tbody>
+			</table>
+			<br><br>
+			<p class="sub-title">Payment</p>
+				<br>
+					<a href="http://www.paypal.com"><p class="p-left">PayPal</p></a>
+				<br><br>
+					<a href="http://www.paypal.com"><img src="../assets/img/front/paypal.png" alt="Paypal Logo"/></a>
+				<br><br>
+					<p>PayPal is a global online payment service.</p>
+					<p>You can use most of the major credit cards including VISA, Master, American Express.</p>
+					<p>Choose either one-time or installment payment.</p>
+					<button class="button">Go to Payment</button>
 		</div>
 	</div>
 </section>
--- fuel/app/views/faq.php
+++ fuel/app/views/faq.php
@@ -1,4 +1,38 @@
-<section id="contents" class="tutor">
-	<div class="content-tutor content-text">
+<section id="contents" class="faq">
+	<div class="content-faq content-text">
+		<div id="content">
+			<p class="sub-title">Frequently Asked Questions</p>
+			<blockquote>I know nothing about programming. Can I start the course?</blockquote>
+			<p class="answer">
+				<strong>(Answer)</strong>
+				Yes. You don’t have to know anything about programming before you start the course. We created our original textbook for a beginner-level student. Our experienced tutors and the textbook help you code a game.
+			</p>
+			<blockquote>What are infrastructures required to start the course?</blockquote>
+			<p class="answer">
+				<strong>(Answer)</strong>
+				You need to have a PC, Internet access, and a handset to communicate with your tutor.
+				For further instruction, refer to Course Information you will receive after registering your profile. It shows you how to prepare the lesson step by step.
+			</p>
+			<blockquote>Who teaches the course?</blockquote>
+			<p class="answer">
+				<strong>(Answer)</strong>
+				Your tutors are Filipino experienced programmers who have been engaged in various programming projects to date. They are very much motivated to educate students and teach you how to think and code a game.
+			</p>
+			<blockquote>When do I take an online private lesson?</blockquote>
+			<p class="answer">
+				<strong>(Answer)</strong>
+				We provide an online private lesson from 10:00 to 24:00 (Philippines time) Monday through Friday. So, lesson time depends on where you live. You can choose a lesson time that is the most convenient for you from tutors’ schedule.
+			</p>
+			<blockquote>How do I communicate with tutor during the lesson?</blockquote>
+			<p class="answer">
+				<strong>(Answer)</strong>
+				You communicate with tutor using Google Hangout to talk and share your desktop screen of your PC. If you have a problem with Google Hangout, you then switch to use Skype.
+			</p>
+			<blockquote>How can I withdraw from the course after I am enrolled?</blockquote>
+			<p class="answer">
+				<strong>(Answer)</strong>
+				If you would like to withdraw from the course, please follow this link. <a href="#">Withdraw</a>
+			</p>
+		</div>
 	</div>
 </section>
