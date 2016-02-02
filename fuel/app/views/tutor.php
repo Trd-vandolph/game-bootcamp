45e1aa690c74c3b26a0e18e365a76ece19b0e0d5
trd-leoalexander <trd.leoalexander@gmail.com>
2016-02-02 16:40:18 +0800

    add tutor felman and john alex

--- fuel/app/views/tutor.php
+++ fuel/app/views/tutor.php
@@ -4,15 +4,6 @@
 		<ul class="tutors_list_l clearfix">
 			<li>
 				<div class="photo">
-					<img src="/assets/img/front/tutor_01.png" alt="tutor photo">
-				</div>
-				<h4>Vandolph C. Reyes</h4>
-				<div class="text">
-					Van graduated from Southwestern University with a diploma of Bachelor of Science Major in Information Technology. He was awarded as Researcher of the Year, elected as Ambassador President for the whole College of Computer Studies, and consistently on the Dean's list. After graduation, he became a software engineer specializing in HTML, CSS and JavaScript and has worked on multiple projects of Web application. He is into sports, well rounded, flexible, a good communicator, and a person you can easily get along with.
-				</div>
-			</li>
-			<li>
-				<div class="photo">
 					<img src="/assets/img/front/tutor_03.jpg" alt="tutor photo">
 				</div>
 				<h4>Leo Alexander Suganob</h4>
@@ -20,16 +11,25 @@
 					Leo graduated from System Technology Institute with a Diploma in Bachelor of Science in Information Technology. He was once awarded as an Outstanding Senior Programmer of the year by leading a group of programmers during their thesis proposal and was on the Dean's list since his first year of college until he graduated. As a computer enthusiast, he keep seeking and exploring more knowledge about computers, especially in web development. He works as a Web Developer in the Philippines.
 				</div>
 			</li>
+			<li>
+				<div class="photo">
+                    <img src="/assets/img/front/tutor_05.png" alt="tutor photo">
+                </div>
+                <h4>Felman Buntog</h4>
+                <div class="text">
+                    Felman graduated from AMA Computer Learning Center Bukidnon with a diploma of Bachelor of Science in Information Technology. He was awarded a Special Service Award in college for having been an active member of a Student Society of Programmers and Designers that aims to help and advocate Technology awareness to fellow students. As a tech guy, he is curious and loves to hack on many things specially on computers. He is a software developer and has worked on many projects on different platforms.
+                </div>
+			</li>
 		</ul>
 		<ul class="tutors_list_l clearfix">
 			<li>
 				<div class="photo">
-					<img src="/assets/img/front/tutor_04.png" alt="tutor photo">
-				</div>
-				<h4>Jovilyn Abella</h4>
-				<div class="text">
-					Jovi graduated from Cebu Technological University with a diploma of Bachelor of Information and Communication Technology. She was on the Dean's list since her first year of college until she graduated. She works as a Web developer and has much experiences in IT projects at diverse companies. She likes reading and exploring new web practices to keep track on the trendy topics. During her free time, she enjoys art activities such as drawing and painting. Once a month, she joins a run-for-a-cause activity to help those in-need.
-				</div>
+                    <img src="/assets/img/front/tutor_06.png" alt="tutor photo">
+                </div>
+                <h4>John Alex Landra</h4>
+                <div class="text">
+                    Alex graduated from University of Cebu with a diploma of Bachelor of Science in Computer Engineering and is focusing on Software Engineering, Web Development, and Mobile Applications. He has background knowledge in programming languages such as C#, Java, C++, Ruby, VB/.Net. Mobile App: Android and Object C. Web and Scripting: HTML, CSS, JavaScript, PHP, ASP and Ruby on Rails. He is also an active member and Co-founder of an anonymous group of Software Engineers and Programmers. His passion in computing and programming leads him to his dream career.
+                </div>
 			</li>
 		</ul>
 	</div>
